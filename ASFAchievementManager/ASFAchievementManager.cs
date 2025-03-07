cc6fe9d6ad40d239c699c5b25a3023ed8a66cf52
Jackie Ryan <jackiewaltryan@gmail.com>
2025-03-07 22:35:34 +0200

    Update ASFAchievementManager.cs

--- ASFAchievementManager/ASFAchievementManager.cs
+++ ASFAchievementManager/ASFAchievementManager.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Composition;
+using System.Threading;
 using System.Threading.Tasks;
 using ArchiSteamFarm.Core;
 using ArchiSteamFarm.Steam;
